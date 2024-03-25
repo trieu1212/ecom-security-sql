34bde98ffba42b1d025c64971e6239f0d79d9aad
trieu <lequoctrieu618@gmail.com>
2024-03-25 22:39:22 +0700

    add constraint delete category fronted admin page

--- fe/src/components/ADMIN-COMPONENTS/CategoryModal/CategoryModal.jsx
+++ fe/src/components/ADMIN-COMPONENTS/CategoryModal/CategoryModal.jsx
@@ -4,6 +4,7 @@ import Modal from "react-bootstrap/Modal";
 import { useDispatch, useSelector } from "react-redux";
 import { createAxios } from "../../../services/axiosJWT";
 import { loginSuccess } from "../../../redux/authSlice";
+import { toast } from "react-toastify";
 function CategoryModal(props) {
   const { show, setShow, title, categoryId } = props;
   const [name, setName] = useState("");
@@ -13,27 +14,29 @@ function CategoryModal(props) {
   let axiosJWT = createAxios(user, dispatch, loginSuccess, user?.refreshToken);
   const handleClose = () => setShow(false);
   useEffect(() => {
-    if(show && title === "Sửa danh mục sản phẩm"){
-        const getCategory = async () => {
-            const res = await axiosJWT.get(`http://localhost:7000/api/category/${categoryId}/${user?.id}`,{
-                headers: {
-                    Authorization: `Bearer ${user?.accessToken}`,
-                },
-            });
-            setName(res.data.name)
-            setDescription(res.data.description)
-        }
-        getCategory()
-    }
-    else return;
-  },[show,title,categoryId,user])
+    if (show && title === "Sửa danh mục sản phẩm") {
+      const getCategory = async () => {
+        const res = await axiosJWT.get(
+          `http://localhost:7000/api/category/${categoryId}/${user?.id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user?.accessToken}`,
+            },
+          }
+        );
+        setName(res.data.name);
+        setDescription(res.data.description);
+      };
+      getCategory();
+    } else return;
+  }, [show, title, categoryId, user]);
   const handleAction = async () => {
     if (title === "Thêm danh mục sản phẩm") {
       const data = {
         name: name,
         description: description,
       };
-      await axiosJWT.post(
+      const res = await axiosJWT.post(
         `http://localhost:7000/api/category/create/${user?.id}`,
         data,
         {
@@ -42,24 +45,34 @@ function CategoryModal(props) {
           },
         }
       );
+      toast.success(res.data.message);
       setShow(false);
     } else if (title === "Xóa danh mục sản phẩm") {
-      await axiosJWT.delete(
-        `http://localhost:7000/api/category/delete/${categoryId}/${user?.id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${user?.accessToken}`,
-          },
-        }
-      );
-      setShow(false);
-    }
-    else if (title === "Sửa danh mục sản phẩm") {
+      try {
+        const response = await axiosJWT.delete(
+          `http://localhost:7000/api/category/delete/${categoryId}/${user?.id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user?.accessToken}`,
+            },
+          }
+        );
+        toast.success(response.data.message);
+      } catch (error) {
+        if (error.response && error.response.data && error.response.data.message) {
+          toast.error(error.response.data.message);
+      } else {
+          console.error("Error:", error);
+          toast.error("Có lỗi xảy ra khi kết nối đến máy chủ.");
+      } }finally {
+        setShow(false);
+      }
+    } else if (title === "Sửa danh mục sản phẩm") {
       const data = {
         name: name,
         description: description,
       };
-      await axiosJWT.put(
+      const res = await axiosJWT.put(
         `http://localhost:7000/api/category/update/${categoryId}/${user?.id}`,
         data,
         {
@@ -68,6 +81,7 @@ function CategoryModal(props) {
           },
         }
       );
+      toast.success(res.data.message);
       setShow(false);
     }
   };
@@ -104,7 +118,11 @@ function CategoryModal(props) {
               </form>
             </>
           ) : null}
-          {title === "Xóa danh mục sản phẩm" ? (<><h3>Bạn có chắc là muốn xóa danh mục này không?</h3></>):null}
+          {title === "Xóa danh mục sản phẩm" ? (
+            <>
+              <h3>Bạn có chắc là muốn xóa danh mục này không?</h3>
+            </>
+          ) : null}
           {title === "Sửa danh mục sản phẩm" ? (
             <>
               <form>
@@ -130,7 +148,7 @@ function CategoryModal(props) {
                 </div>
               </form>
             </>
-          ):null}
+          ) : null}
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
